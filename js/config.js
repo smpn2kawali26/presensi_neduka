f982028b058043938c752a11560fa9e1b1ab9fd6
smpn2kawali26 <sidabarneduka26@gmail.com>
2026-08-02 08:31:42 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,31 +3,24 @@
  * File: js/config.js
  *****************************************************************/
 
+const SCRIPT_URL_REAL = "https://script.google.com/macros/s/AKfycbyR5J64U2HYHtUDaVuCAUH73KMcKrnVhavhgIrJbTViflsql7pcqNf2gD6DHxOAdNn-/exec";
+
 const CONFIG = {
-    // 1. NAMA APLIKASI & SEKOLAH
     APP_NAME: "Portal Absensi Digital",
     SCHOOL_NAME: "SMP Negeri 2 Kawali",
-    
-    // 2. URL GOOGLE APPS SCRIPT (BACKEND REAL BARU)
-    BASE_URL: "https://script.google.com/macros/s/AKfycbyR5J64U2HYHtUDaVuCAUH73KMcKrnVhavhgIrJbTViflsql7pcqNf2gD6DHxOAdNn-/exec",
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyR5J64U2HYHtUDaVuCAUH73KMcKrnVhavhgIrJbTViflsql7pcqNf2gD6DHxOAdNn-/exec",
-    API_URL: "https://script.google.com/macros/s/AKfycbyR5J64U2HYHtUDaVuCAUH73KMcKrnVhavhgIrJbTViflsql7pcqNf2gD6DHxOAdNn-/exec",
-
-    // 3. LOGO
+    BASE_URL: SCRIPT_URL_REAL,
+    SCRIPT_URL: SCRIPT_URL_REAL,
+    API_URL: SCRIPT_URL_REAL,
     LOGO_URL: "logo.png",
-
-    // 4. TIMEOUT
     TIMEOUT_MS: 15000,
-
-    // 5. HELPER API URL
     getApiUrl: function (action = "") {
-        return action ? `${this.BASE_URL}?action=${action}` : this.BASE_URL;
+        return action ? `${SCRIPT_URL_REAL}?action=${action}` : SCRIPT_URL_REAL;
     }
 };
 
-// Mencegah error jika file lain mencari variabel window.CONFIG atau CONFIG_URL
+// Global Variable Fallbacks
 window.CONFIG = CONFIG;
-window.CONFIG_URL = CONFIG.BASE_URL;
-window.SCRIPT_URL = CONFIG.BASE_URL;
-
-Object.freeze(CONFIG);
+window.CONFIG_URL = SCRIPT_URL_REAL;
+window.SCRIPT_URL = SCRIPT_URL_REAL;
+window.BASE_URL = SCRIPT_URL_REAL;
+window.API_URL = SCRIPT_URL_REAL;
